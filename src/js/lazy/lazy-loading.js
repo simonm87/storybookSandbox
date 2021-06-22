87cbd406728e45a0f00293f9e57d8c2ec1cd2279
Simon Mumby <simonmumby@woodlandtrust.org.uk>
2021-06-22 19:34:05 +0100

    Do no lazy load in react

--- src/js/lazy/lazy-loading.js
+++ src/js/lazy/lazy-loading.js
@@ -1,29 +1,38 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     var lazyImages = [].slice.call(document.querySelectorAll(".lazy"));
-    if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
-        // LazyLoad images using IntersectionObserver
-        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
-            entries.forEach(function (entry) {
-                if (entry.isIntersecting) {
-                    let lazyImage = entry.target;
-                    lazyImage.src = lazyImage.dataset.src;
-                    lazyImage.srcset = lazyImage.dataset.srcset;
-                    lazyImage.classList.remove("lazy");
-                    lazyImageObserver.unobserve(lazyImage);
-                }
-            });
-        });
 
-        lazyImages.forEach(function (lazyImage) {
-            lazyImageObserver.observe(lazyImage);
-        });
+    //if ("IntersectionObserver" in window && "IntersectionObserverEntry" in window && "intersectionRatio" in window.IntersectionObserverEntry.prototype) {
+    //    // LazyLoad images using IntersectionObserver
+    //    let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+    //        entries.forEach(function (entry) {
+    //            if (entry.isIntersecting) {
+    //                let lazyImage = entry.target;
+    //                lazyImage.src = lazyImage.dataset.src;
+    //                lazyImage.srcset = lazyImage.dataset.srcset;
+    //                lazyImage.classList.remove("lazy");
+    //                lazyImageObserver.unobserve(lazyImage);
+    //            }
+    //        });
+    //    });
 
-    } else {
-        // Load all high res images at once if browser is not supported
-        lazyImages.forEach(function (lazyImage) {
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.srcset = lazyImage.dataset.srcset;
-            lazyImage.classList.remove("lazy");
-        });
-    }
+    //    lazyImages.forEach(function (lazyImage) {
+    //        lazyImageObserver.observe(lazyImage);
+    //    });
+
+    //} else {
+    //    // Load all high res images at once if browser is not supported
+    //    lazyImages.forEach(function (lazyImage) {
+    //        lazyImage.src = lazyImage.dataset.src;
+    //        lazyImage.srcset = lazyImage.dataset.srcset;
+    //        lazyImage.classList.remove("lazy");
+    //    });
+    //}
+
+    // Do not lazy load in react app
+    lazyImages.forEach(function (lazyImage) {
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.srcset = lazyImage.dataset.srcset;
+        lazyImage.classList.remove("lazy");
+    });
+
 });
